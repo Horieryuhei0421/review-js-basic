3e2a2bba778fb9ea1e780c68ea57b323e08c8b46
iniad <iniad@MacBook-Pro-7.local>
2022-01-02 21:28:58 +0900

    carousel

--- js/main.js
+++ js/main.js
@@ -99,4 +99,75 @@
     });
   });
 
+
+
+  const next = document.getElementById('next');
+  const prev = document.getElementById('prev');
+  const u = document.getElementById('u');
+  const slides = u.children;
+  const dots = [];
+  let currentIndex = 0;
+
+  function updateButtons() {
+    prev.classList.remove('c-hidden');
+    next.classList.remove('c-hidden');
+
+    if (currentIndex === 0) {
+      prev.classList.add('c-hidden');
+    }
+    if (currentIndex === slides.length - 1) {
+      next.classList.add('c-hidden');
+    }
+  }
+
+  function moveSlides() {
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    u.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
+  }
+
+  function setupDots() {
+    for (let i = 0; i < slides.length; i++) {
+      const button = document.createElement('button');
+      button.addEventListener('click', () => {
+        currentIndex = i;
+        updateDots();
+        updateButtons();
+        moveSlides();
+      });
+      dots.push(button);
+      document.getElementById('calnav').appendChild(button);
+    }
+
+    dots[0].classList.add('current');
+  }
+
+  function updateDots() {
+    dots.forEach(dot => {
+      dot.classList.remove('current');
+    });
+    dots[currentIndex].classList.add('current');
+  }
+
+  updateButtons();
+  setupDots();
+
+  next.addEventListener('click', () => {
+    currentIndex++;
+    updateButtons();
+    updateDots();
+    moveSlides();
+  });
+
+  prev.addEventListener('click', () => {
+    currentIndex--;
+    updateButtons();
+    updateDots();
+    moveSlides();
+  });
+
+  window.addEventListener('resize', () => {
+    moveSlides();
+  });
+
+
 }
