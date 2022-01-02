ec480867b64a6496aac6693dc83996f5a8b2c24a
iniad <iniad@MacBook-Pro-7.local>
2022-01-02 12:05:03 +0900

    modal

--- js/main.js
+++ js/main.js
@@ -25,4 +25,27 @@
   targets.forEach(target => {
     observer.observe(target);
   });
+
+  // ƒ‚[ƒ_ƒ‹
+  const open = document.getElementById('open');
+  const close = document.getElementById('close');
+  const modal = document.getElementById('modal');
+  const mask = document.getElementById('mask');
+
+  open.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+
+  close.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+
+  mask.addEventListener('click', () => {
+    // modal.classList.add('hidden');
+    // mask.classList.add('hidden');
+    close.click();
+  });
+
 }
